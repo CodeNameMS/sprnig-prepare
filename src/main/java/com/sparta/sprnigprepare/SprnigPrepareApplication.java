317110434b95a54d153c0b9c34d60b31eb71ade8
minseok <alstjrdlrk12@naver.com>
2023-11-03 13:35:52 +0900

    init commit

---
+++ src/main/java/com/sparta/sprnigprepare/SprnigPrepareApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sprnigprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprnigPrepareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprnigPrepareApplication.class, args);
+    }
+
+}
